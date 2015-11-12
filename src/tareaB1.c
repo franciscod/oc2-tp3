042d777b0d9ab0dafbe048cb30cf18b031fd606f
Francisco Demartino <demartino.francisco@gmail.com>
2015-11-12 06:09:07 -0300

    perros para debuggear

--- src/tareaB1.c
+++ src/tareaB1.c
@@ -7,25 +7,13 @@
 #include "game.h"
 #include "syscall.h"
 
-// ejemplo de uso de memoria compartida para la guerra de tareas:
-// parte (1)
-// declaramos un stuct que va a guardar datos en 0x401000
-//typedef struct extra_t {
-// las variables de instancia pueden ser lo que queramos
-//	char mapa[80*50];
-//	int next_id;
-//	int huesos[8][2];
-//	
-//} extra_t;
-
 
 void task(int x, int y) {
-    /* Tarea */
+    // Perro "CORRO Y ME CUELGO"
 
-// parte (2)
-//	extra_t *extra = (extra_t*)0x401000;
-// a partir de aqui tenemos la estructura disponible 
-
-
+    int i = 20;
+    while(i--){
+        syscall_moverse(ARR);
+    }
     while(1) { __asm __volatile("mov $2, %%eax":::"eax"); }
 }
--- src/tareaB2.c
+++ src/tareaB2.c
@@ -7,7 +7,36 @@
 #include "game.h"
 #include "syscall.h"
 
+void wait(int pseudosecs);
+
 void task(int x_target, int y_target) {
-    /* Tarea */
-  while(1) { __asm __volatile("mov $2, %%eax":::"eax"); }
+	// Perro "BAILE SUICIDA"
+	syscall_moverse(IZQ);wait(100);
+	syscall_moverse(IZQ);wait(100);
+	syscall_moverse(IZQ);wait(100);
+	syscall_moverse(IZQ);wait(100);
+	syscall_moverse(IZQ);wait(100);
+	syscall_moverse(IZQ);wait(100);
+
+	int d = 400;
+	while (d>2) {
+		syscall_moverse(ARR); wait(d);
+		syscall_moverse(ABA); wait(d);
+		d= d / 10 * 4;
+	}
+
+	*((int*) 1) = 123; // BOOM!
+
+	// esto no va a correr nunca porque lo anterior me desaloja
+	syscall_moverse(IZQ);wait(100);
+	syscall_moverse(IZQ);wait(100);
+	syscall_moverse(IZQ);wait(100);
+	while(1) { __asm __volatile("mov $2, %%eax":::"eax"); }
+}
+
+
+void wait(int pseudoms)
+{
+	int count;
+	for (count = 0; count < pseudoms * 1000; count++) {}
 }
